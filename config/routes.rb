89843254c36a0826fb1f216caec8190eb748a34d
善郎 嶺山 <mineyama358@gmail.com>
2025-12-14 13:38:07 +0900

    Remove duplicate routes and keep RESTful routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get "users/new"
-  get "users/create"
-  get "static_pages/top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
